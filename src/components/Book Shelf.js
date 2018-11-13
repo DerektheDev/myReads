c0c183cd2a75bb97ae1862c3dc00ae6ba90bfde8
Derek Montgomery <derekm@ifit.com>
2018-11-12 21:06:13 -0600

    implement null check

--- src/components/Book Shelf.js
+++ src/components/Book Shelf.js
@@ -5,22 +5,21 @@ class BookShelf extends React.Component {
 state ={}
 
 render(){
+  const books = this.props.shelf.books;
+
   return(
    <div>
     <div className="bookshelf">
       <h2 className="bookshelf-title">Currently Reading</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-        {this
-          .props
-          .shelf
-          .books
-          .map(book => (
-            <li key={book.id}>
-           <Book  book ={book} />
-          </li>
-       ))
-      }
+          {
+            books && books.map(book => (
+              <li key={book.id}>
+                <Book  book ={book} />
+              </li>
+            ))
+          }
         </ol>
       </div>
     </div>
